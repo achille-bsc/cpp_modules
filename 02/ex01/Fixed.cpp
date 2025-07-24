3bf056a5746c2ca9b79d9a05d792562f1ffbc5a3
achille-bsc <achillebosc@gmail.com>
2025-07-24 10:48:53 +0200

    ex00 & ex01 works completely

---
+++ 02/ex01/Fixed.cpp
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abosc <abosc@student.42lehavre.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 18:40:59 by abosc             #+#    #+#             */
+/*   Updated: 2025/07/24 10:46:56 by abosc            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//////////////////////////////////
+//                              //
+//            HEADER            //
+//                              //
+//////////////////////////////////
+
+#include "Fixed.hpp"
+#include <cmath>
+
+
+//////////////////////////////////
+//                              //
+//         CONSTRUCTORS         //
+//                              //
+//////////////////////////////////
+
+const int Fixed::fractionnalBits = 8;
+
+Fixed::Fixed(): fpValue(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int fixedPoint)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->fpValue = fixedPoint * (1 << this->fractionnalBits);
+}
+Fixed::Fixed(const float floatingPoint)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->fpValue = roundf(floatingPoint * (1 << this->fractionnalBits));
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+
+//////////////////////////////////
+//                              //
+//            GETTER            //
+//                              //
+//////////////////////////////////
+
+int		Fixed::getFixedPoint(void) const
+{
+	return (this->fpValue);
+}
+
+
+//////////////////////////////////
+//                              //
+//            SETTER            //
+//                              //
+//////////////////////////////////
+
+void	Fixed::setFixedPoint(int const fixedPoint)
+{ 
+	this->fpValue = fixedPoint;
+}
+
+
+//////////////////////////////////
+//                              //
+//           METHODS            //
+//                              //
+//////////////////////////////////
+
+float	Fixed::toFloat(void)	const
+{
+	return ((float)this->fpValue / (float)(1 << this->fractionnalBits));
+}
+
+int		Fixed::toInt(void)		const
+{
+	return (this->fpValue >> this->fractionnalBits);
+}
+
+
+//////////////////////////////////
+//                              //
+//          OVERLOADS           //
+//                              //
+//////////////////////////////////
+
+Fixed& Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		this->fpValue = other.getFixedPoint();
+	return (*this);
+}
+
+std::ostream    &operator<<(std::ostream &o, Fixed const &fixed)
+{
+	o << fixed.toFloat();
+	return (o);
+}
---
+++ 02/ex01/Fixed.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abosc <abosc@student.42lehavre.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/17 18:41:02 by abosc             #+#    #+#             */
+/*   Updated: 2025/07/24 10:23:37 by abosc            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+# define FIXED_H
+# include <iostream>
+class Fixed
+{
+	private:
+		int					fpValue;
+		static const int	fractionnalBits;
+		int					getFixedPoint(void) const;
+		void				setFixedPoint(int const fixedPoint);
+	public:
+		Fixed				();
+		Fixed				(const int		fixedPoint);
+		Fixed				(const float	floatingPoint);
+		Fixed				(const Fixed& other);
+		~Fixed				();
+		Fixed&				operator= (const Fixed& other);
+		float				toFloat(void)	const;
+		int					toInt(void)		const;
+};
+
+std::ostream    &operator<<(std::ostream &os, Fixed const &fixed);
+
+#endif
